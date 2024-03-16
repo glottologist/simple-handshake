31aef9f7a052f2af8ced8b71afdc9c8f69a23f24
jason <jason@ridgway-taylor.co.uk>
2024-03-16 17:03:34 +0700

    feat: added additional comments

--- src/solana/transport/mod.rs
+++ src/solana/transport/mod.rs
@@ -1,18 +1,32 @@
+// Imports the `async_trait` macro for enabling asynchronous methods within traits,
+// and the standard `io` module for input/output operations, including network communication and error handling.
 use {async_trait::async_trait, std::io};
+
+// Module declarations for TCP and WebSocket implementations,
+// allowing for specific transport protocol functionality to be encapsulated within these modules.
 pub mod tcp;
 pub mod ws;
 
+// Define the `Transport` trait for asynchronous network communication.
+// This trait provides a generic interface for sending data across a network and receiving a response.
 #[async_trait]
 pub trait Transport: Send + Sync {
+    // Ensure the trait objects are safe to send and share across threads.
+    // An asynchronous method that attempts to connect to a remote endpoint, send a payload,
+    // and await a response. It accepts an optional timeout and a JSON payload as parameters.
     async fn connect_and_send(
         &self,
-        timeout: Option<u32>,
-        payload: serde_json::Value,
-    ) -> io::Result<String>;
+        timeout: Option<u32>,       // Optional timeout in seconds.
+        payload: serde_json::Value, // The payload to be sent, encapsulated as JSON.
+    ) -> io::Result<String>; // Returns an `io::Result` encapsulating the response as a `String` or an error.
 }
 
+// Define the `ChooseTransport` trait for selecting the appropriate transport mechanism at runtime.
 pub trait ChooseTransport {
+    // A method that returns an instance of a type implementing the `Transport` trait, encapsulated in a `Box<dyn Transport>`.
+    // This allows for dynamic dispatch to different transport implementations.
     fn get_transport(&self) -> Box<dyn Transport>
     where
-        Self: Sized;
+        Self: Sized; // Ensures this method can only be called on types that are sized, allowing for self-references.
 }
+
